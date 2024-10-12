cab8c863781850b20ab675a581d1caaf7b719d98
Yaroslav Bilokrylets <yaroslav.bilokrylets@gmail.com>
2024-10-12 13:56:28 +0300

    feat: add LinkButton component

---
+++ app/_components/LinkButton.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+type Props = {
+  href: string;
+  disabled: boolean;
+  children: string;
+};
+export default function LinkButton({ href, disabled, children }: Props) {
+  return (
+    <Link href={disabled ? '#' : href}>
+      <button
+        className=" disabled:cursor-not-allowed border text-sm rounded-lg  block w-auto py-2.5 px-5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white hover:ring-blue-500 hover:border-blue-500"
+        disabled={disabled}
+      >
+        {children}
+      </button>
+    </Link>
+  );
+}
